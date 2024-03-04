1a54f93434e7d7ec2e43ae4c2dfd59f32a8bb899
Felipe Rivera <felipe.rivera.9803@gmail.com>
2024-03-03 23:59:37 -0800

    Initial Commit

---
+++ WinMan/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WinMan
+//
+//  Created by Felipe Rivera on 3/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WinMan/WinManApp.swift
@@ -0,0 +1,17 @@
+//
+//  WinManApp.swift
+//  WinMan
+//
+//  Created by Felipe Rivera on 3/3/24.
+//
+
+import SwiftUI
+
+@main
+struct WinManApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
