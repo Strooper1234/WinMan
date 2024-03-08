c44cf537f7ed89f4368b18aecf39f6b0ff83ada6
Felipe Rivera <felipe.rivera.9803@gmail.com>
2024-03-08 00:45:12 -0800

    previewWindow Manager created and the view and its toggle class, this also include moving the window around the grid and setting up the actual window when Super Shortcut is release

--- WinMan/previewWindow.swift
+++ WinMan/previewWindow.swift
@@ -32,3 +32,122 @@ struct GridLocation {
         return self.numRow > 0 || self.numCol > 0
     }
 }
+enum PreviewState {
+    case outOfGrid
+    case insideGrid
+}
+
+class PreviewWindowManager: ObservableObject {
+    static let shared = PreviewWindowManager()
+    @Published var gridLocation = GridLocation()
+    
+    var pos: CGPoint = CGPoint()
+    var size: CGSize = CGSize()
+    var state = PreviewState.outOfGrid
+    
+    var isTouchingLeftEdge: Bool {
+        return self.gridLocation.originCol == 0
+    }
+    var isTouchingRightEdge: Bool {
+        return self.gridLocation.endCol == TileManager.shared.cols
+    }
+    var isTouchingTopEdge: Bool {
+        return self.gridLocation.originRow == 0
+    }
+    var isTouchingBottomEdge: Bool {
+        return self.gridLocation.endRow == TileManager.shared.rows
+    }
+    func start(_ window: Window?) {
+//        TODO: check if the window is already managed by the TileManager
+        if let window = window {
+            self.pos = window.position
+            self.size = window.size
+        }
+        
+        
+        PreviewWindowToggleController.shared.toggleOverlayOn()
+//        calcRowsColsNeeded(window)
+//        print(self.gridLocation.numCol, self.gridLocation.numCol)
+    }
+    
+    func insertLeft(_ window: Window) {
+        calcRowsColsNeeded(window)
+        self.state = .insideGrid
+    }
+    
+    func moveRight() {
+        if self.isTouchingRightEdge {return}
+        gridLocation.originCol += 1
+    }
+    func moveLeft() {
+        if self.isTouchingLeftEdge {return}
+        gridLocation.originCol -= 1
+    }
+    func moveDown() {
+        if self.isTouchingBottomEdge {return}
+        gridLocation.originRow += 1
+    }
+    func moveUp() {
+        if self.isTouchingTopEdge {return}
+        gridLocation.originRow -= 1
+    }
+    
+    func calcRowsColsNeeded(_ window: Window) {
+        self.gridLocation.numCol = Int(window.size.width) / TileManager.shared.cellWidth
+        self.gridLocation.numRow = Int(window.size.height) / TileManager.shared.cellHeight
+    }
+    
+    func end() {
+        PreviewWindowToggleController.shared.toggleOverlayOff()
+        if gridLocation.hasSize {
+            let window = WindowManager.frontmostWindow()
+            window?.setSize(size: self.gridLocation.size)
+            window?.setPosition(position: self.gridLocation.pos)
+        }
+        self.gridLocation = GridLocation()
+        self.state = .outOfGrid
+    }
+    
+}
+
+struct PreviewWindowView: View {
+    @ObservedObject private var preWinMan = PreviewWindowManager.shared
+    
+    private var size: CGSize {
+        return preWinMan.gridLocation.hasSize ? preWinMan.gridLocation.size : preWinMan.size
+    }
+    private var pos: CGPoint {
+        return preWinMan.gridLocation.hasSize ? preWinMan.gridLocation.pos : preWinMan.pos
+    }
+    
+    var body: some View {
+        GeometryReader { geometry in
+            RoundedRectangle(cornerRadius: 15)
+                .stroke(Color.orange, lineWidth: 3)
+                .fill(Color.black.opacity(0.4))
+                .frame(width: size.width, height: size.height)
+                .offset(x: pos.x, y: pos.y)
+//                .position(x: preRect.origin.x, y: preRect.origin.y)
+        }
+    }
+}
+
+class PreviewWindowToggleController: ObservableObject {
+    static let shared = PreviewWindowToggleController()
+    let previewController = PreviewController()
+    
+    func toggleOverlay() {
+        if previewController.isOpen {
+            previewController.close()
+        } else {
+            previewController.open(screen: NSScreen.main!, view: PreviewWindowView())
+        }
+
+    }
+    func toggleOverlayOn() {
+        previewController.open(screen: NSScreen.main!, view: PreviewWindowView())
+    }
+    func toggleOverlayOff() {
+        previewController.close()
+    }
+}
