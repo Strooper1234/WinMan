82ddee1be9a0b917b30d910d63c545ccb9bcba09
Felipe Rivera <felipe.rivera.9803@gmail.com>
2024-03-08 00:38:51 -0800

    GridLocation added to keep track of the next feature, the preview window

--- WinMan/previewWindow.swift
+++ WinMan/previewWindow.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct GridLocation {
+    var numRow = 0
+    var numCol = 0
+    var originRow = 0
+    var originCol = 0
+    
+    var endRow: Int {
+        return self.originRow + self.numRow
+    }
+    var endCol: Int {
+        return self.originCol + self.numCol
+    }
+    
+    var size: CGSize {
+        return CGSize(width: self.numCol * TileManager.shared.cellWidth, height: self.numRow * TileManager.shared.cellHeight)
+    }
+    var pos: CGPoint {
+        return CGPoint(x: self.originCol * TileManager.shared.cellWidth, y: self.originRow * TileManager.shared.cellHeight)
+    }
+    
+    var hasSize: Bool {
+        return self.numRow > 0 || self.numCol > 0
+    }
+}
